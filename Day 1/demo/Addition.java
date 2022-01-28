c0580ee81f7ef791cf8c178042a2e3060a9231dd
Janaki01 <janakimuthuluri@gmail.com>
2022-01-28 18:27:05 +0530

    my day1 java prgs

---
+++ Day 1/demo/Addition.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Addition {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int temp=a+b;
+		int temp1=a-b;
+		System.out.println("sum of two numbers-->"+temp);
+		System.out.println("sub of two numbers-->"+temp1);
+	}
+}
