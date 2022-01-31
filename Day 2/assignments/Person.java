34b4d2005c21c8bd48c8d47d9ff2ccd573d0b12b
Janaki01 <janakimuthuluri@gmail.com>
2022-01-31 22:23:16 +0530

    My Day2 Java Programs

---
+++ Day 2/assignments/Person.java
@@ -0,0 +1,12 @@
+package com.demo.assignments;
+
+public class Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee employee = new Employee("Janaki", 400000, 2022, "3fw64r321ad");
+	    employee.PrintDetails();
+
+	}
+
+}
