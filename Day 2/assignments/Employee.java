0f6d7c0a5982e61e24645b240719d4d5dbc67a75
Janaki01 <janakimuthuluri@gmail.com>
2022-02-01 10:12:35 +0530

    My Day2 Java Programs

--- Day 2/assignments/Employee.java
+++ Day 2/assignments/Employee.java
@@ -22,7 +22,7 @@ class Employee extends Person {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Employee employee = new Employee("Gayathri", 400000, 2022, "3fw64r321ad");
+		Employee employee = new Employee("Janaki", 50000, 2022, "ghfdfb");
 	    employee.PrintDetails();
 
 	}
--- Day 2/assignments/Person.java
+++ Day 2/assignments/Person.java
@@ -4,7 +4,7 @@ public class Person {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Employee employee = new Employee("Janaki", 400000, 2022, "3fw64r321ad");
+		Employee employee = new Employee("Janaki", 50000, 2022, "ghfdfb");
 	    employee.PrintDetails();
 
 	}
