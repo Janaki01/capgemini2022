23693c01e67bdbe9958ed1340d379e9a1f2fe07e
Janaki01 <janakimuthuluri@gmail.com>
2022-02-02 19:20:38 +0530

    My Day4 Java Programs

---
+++ Day 4/Divisibleby235.java
@@ -0,0 +1,20 @@
+package com.demo.Day4;
+import java.util.Scanner;
+public class Divisibleby235 {
+
+    	public static void main(String args[]) {
+    			
+    		        int i=5,count=0;
+    		        while(true)
+    		        {
+    		            if(i%2==0&&i%3==0&&i%5==0)
+    		            {
+    		                System.out.print(i+" ");
+    		                count++;
+    		            }
+    		            i++;
+    		            if(count==5)
+    		            break;
+    		        }
+    		    }
+    		}
