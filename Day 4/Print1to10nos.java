1f7ee4b19681a6381e41cdc6c53b01562390c9ab
Janaki01 <janakimuthuluri@gmail.com>
2022-02-02 19:10:49 +0530

    My Day4 Java Programs

---
+++ Day 4/Print1to10nos.java
@@ -0,0 +1,11 @@
+package com.demo.Day4;
+
+public class Print1to10nos {
+	public static void main(String[] args)
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+        	System.out.print(i + " ");
+        }
+    }
+}
---
+++ Day 4/UppertoLowervv.java
@@ -0,0 +1,20 @@
+package com.demo.Day4;
+import java.util.Scanner;
+ 
+public class UppertoLowervv {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a character:");
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		char a=s.next().charAt(0);
+        char b;
+        if(Character.isLowerCase(a))
+            b= Character.toUpperCase(a);
+        else
+            b= Character.toLowerCase(a);
+        System.out.println(a+"->"+b);
+
+	}
+
+}
