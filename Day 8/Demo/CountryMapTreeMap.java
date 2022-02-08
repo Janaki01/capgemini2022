1ac62fab022ace0745ab6abfe1acc80ebd17fefe
Janaki01 <janakimuthuluri@gmail.com>
2022-02-08 22:48:11 +0530

    Collections

---
+++ Day 8/Demo/CountryMapTreeMap.java
@@ -0,0 +1,25 @@
+package com.demo.Day8;
+import java.util.HashMap;
+
+import com.demo.Day8.CountryMap;
+
+public class CountryMapTreeMap {
+
+	public static void main(String[] args) {
+		CountryMap countryMap = new CountryMap();
+
+		countryMap.saveCountryCapital("India", "Delhi");
+		countryMap.saveCountryCapital("Japan", "Tokyo");
+		countryMap.saveCountryCapital("USA", "Washington, D.C.");
+		
+		System.out.println(countryMap.getCapital("India"));
+		System.out.println(countryMap.getCountry("Tokyo"));
+		System.out.println(countryMap.toArrayList());
+		
+		HashMap<String, String> M2 = countryMap.swapKyeValue();
+		System.out.println(M2);
+	}
+
+
+
+}
---
+++ Day 8/Demo/CountryNames.java
@@ -0,0 +1,18 @@
+package com.demo.Day8;
+
+public class CountryNames {
+
+		public static void main(String[] args) {
+			Country countries = new Country();
+			countries.saveCountryNames("India");
+			countries.saveCountryNames("USA");
+			countries.saveCountryNames("Pakistan");
+			countries.saveCountryNames("Bangladesh");
+			countries.saveCountryNames("China");
+
+			System.out.println("India: " + countries.getCountry("India"));
+			System.out.println("Japan: " + countries.getCountry("Japan"));		
+		}
+
+	
+}
