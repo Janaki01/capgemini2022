59204c3a05b35f067f0d1ecc3a200cb654aa0cc6
Janaki01 <janakimuthuluri@gmail.com>
2022-03-03 16:18:55 +0530

    Association Mapping

---
+++ Association Mapping/ManyToOne/src/com/demo/main/MainClass.java
@@ -0,0 +1,13 @@
+package com.demo.main;
+
+import com.demo.logic.Service;
+
+public class MainClass {
+	public static void main(String args[]) {
+		Service serve = new Service();
+		serve.show();
+		System.out.println("Done");
+		
+	}
+
+}
