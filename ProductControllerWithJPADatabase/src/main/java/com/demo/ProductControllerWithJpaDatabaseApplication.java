c9a6f39a0340bf5fb043e4589b531ca5837b8f11
Janaki01 <janakimuthuluri@gmail.com>
2022-03-10 14:35:22 +0530

    spring boot with JPA with h2 database

---
+++ ProductControllerWithJPADatabase/src/main/java/com/demo/ProductControllerWithJpaDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductControllerWithJpaDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductControllerWithJpaDatabaseApplication.class, args);
+	}
+
+}
